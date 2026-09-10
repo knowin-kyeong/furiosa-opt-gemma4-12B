2a44bb36f529ae1125f8391275926470777cc95b
juwon0718 <juwon0718@naver.com>
2026-09-11 01:43:49 +0900

    Park the three rejected attn_out variants; harness back to production vs V251

--- tests/test_kernels.rs
+++ tests/test_kernels.rs
@@ -398,8 +398,8 @@ const FFN_SWEEP: &[&str] = &[""; 3];
 const QKV_SWEEP: &[&str] = &[""; 3];
 
 const ATTN_SWEEP: &[&str] = &[
-    "dx", "nm", "nm", "dx", "dx", "nm", "nm", "dx", "dx", "nm", "nm", "dx",
-    "dx", "nm", "nm", "dx", "dx", "nm", "nm", "dx", "dx", "nm", "nm", "dx",
+    "", "dx", "dx", "", "", "dx", "dx", "", "", "dx", "dx", "",
+    "", "dx", "dx", "", "", "dx", "dx", "", "", "dx", "dx", "",
 ];
 
 const PLAN: &[Plan] = &[
@@ -590,48 +590,6 @@ async fn sliding_attention_output(
             "dx" => {
                 launch(
                     ops::sliding_attention_output_direct,
-                    (
-                        ctx,
-                        &x,
-                        &post_attn_rms_weight,
-                        &o_weight,
-                        &o_weight_scale,
-                        &mut residual,
-                    ),
-                )
-                .await;
-            }
-            "sp" => {
-                launch(
-                    ops::sliding_attention_output_split,
-                    (
-                        ctx,
-                        &x,
-                        &post_attn_rms_weight,
-                        &o_weight,
-                        &o_weight_scale,
-                        &mut residual,
-                    ),
-                )
-                .await;
-            }
-            "gt" => {
-                launch(
-                    ops::sliding_attention_output_gathered,
-                    (
-                        ctx,
-                        &x,
-                        &post_attn_rms_weight,
-                        &o_weight,
-                        &o_weight_scale,
-                        &mut residual,
-                    ),
-                )
-                .await;
-            }
-            "nm" => {
-                launch(
-                    ops::sliding_attention_output_norm,
                     (
                         ctx,
                         &x,
